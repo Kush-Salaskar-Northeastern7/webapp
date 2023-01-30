7c4365d995ca03fe2b710558184ccfed7156ae5a
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-01-29 19:45:34 -0500

    Connected to psql

--- api/app.js
+++ api/app.js
@@ -2,8 +2,18 @@ const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const cookieParser = require('cookie-parser')
+const db  = require('../db_config/db_config')
 
 const app = express()
+
+db.authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 
 app.use(cors())
 app.use(express.json())
--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const app = require('./api/app')
 
 const port = process.env.PORT || 3001
 
-// listens on port 3000
+// listens on port 3001
 app.listen(port, () => {
   console.log(`Web app listening at http://localhost:${port}`)
 })
