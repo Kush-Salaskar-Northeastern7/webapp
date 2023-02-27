17cbec59790746e9a6dd2b82d6a48bc296ab9d71
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-02-27 15:11:56 -0500

    Added test

--- api/controllers/productController.js
+++ api/controllers/productController.js
@@ -194,6 +194,27 @@ const deleteProduct = async (req, res) => {
 
 const getAllImagesForProduct = async (req, res) => {
     try {
+               // the username and password from Basic Auth
+               const requsername = req.credentials.name
+               const reqpassword = req.credentials.pass
+       
+               // pass header username(email) to check if user exists
+               const existingUser = await checkExistingUser(requsername.toLowerCase())
+               if (existingUser == null) return errorHandler(`Username is incorrect`, res, 401)
+       
+               let isPasswordMatch = bcrypt.compareSync(
+                   reqpassword,
+                   existingUser.password
+               );
+        
+               // if wrong password throw 401
+               if (!isPasswordMatch) return errorHandler(`Credentials do not match`, res, 401)
+       
+               const id = req.params.id
+               const productById = await getProductById(id)
+               if (productById == null) return errorHandler(`Product by this id does not exists`, res, 404)
+       
+               if (productById.owner_user_id !== existingUser.id) return errorHandler("You are not allowed to delete this resource", res, 403)
         
     } catch (error) {
        errorHandler(error.message, res) 
