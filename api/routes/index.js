435f815743630f4d3e0b54bb6c99df29b60cf3ab
Kush-Salaskar-Northeastern7 <salaskar.k@northeastern.edu>
2023-04-12 23:14:58 -0400

    Final

--- api/routes/index.js
+++ api/routes/index.js
@@ -6,9 +6,9 @@ module.exports = (app) => {
     app.get('/healthz', (req, res) => {
         res.json()
     })
-    app.get('/healthz1', (req, res) => {
-        res.json()
-    })
+    // app.get('/healthz1', (req, res) => {
+    //     res.json()
+    // })
     app.use('/v1/user', userRouter)
     app.use('/v1/product', productRouter)
 
